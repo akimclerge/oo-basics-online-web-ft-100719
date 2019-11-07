f859310412a561b9e4ec9cc4c8bc9aaad9e2e0ab
Akim Clerge <akimclerge@gmail.com>
2019-11-07 03:29:13 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,9 @@
 class Book
   attr_accessor :author, :page_count, :genre
+  attr_reader :title
  
   def initialize(title)
     @title = title
-  end
- 
-  def title
-    @title
   end
  
   def turn_page
@@ -14,7 +11,6 @@ class Book
   end
  
 end
-
 Book.new("And Then There Were None")
 
 
